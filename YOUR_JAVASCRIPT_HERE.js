633bd84751d6cda207f11d420926e86de486c860
jendhordejan <jendhordejan@gmail.com>
2020-01-11 12:42:12 +0100

    added Hero Object, Game logic functions, initial rest function

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -1,1 +1,55 @@
 // Write your JS here
+
+//functions: add all functions here
+//rest function : function to heal your hero
+function rest(person) {
+  console.log('you are inside rest function')
+  console.log(person.name)
+
+
+  //set character health to 10
+  if (person.health<10){
+    person.health = 10;
+      alert('Your health is full and ready to battle')
+  } else if (person.health==10){
+      alert('Your health is still full.')
+    }
+  return person;
+}
+
+
+
+//pickUpItem : function the pick-up items.
+function pickUpItem(a) {
+  /* ... */
+}
+
+//equipWeapon : function to equipt hero weapon
+function equipWeapon(a) {
+  /* ... */
+}
+
+
+
+
+
+
+//Create a hero.
+const hero = {
+  name: 'Rein',
+  class: 'Knight',
+  weapon: {
+    type: 'Sword',
+    damage: 2
+  },
+  attack: 10,
+  health: 10,
+  heroic: true,
+  inventory: []
+}
+
+console.log(`hero health before rest: ${hero.health}`)
+//for testing
+//hero.health=8;
+rest(hero);
+console.log(hero.health)
