e37bfd9a9a57aba821ea470a59d774ec3c684a8e
jendhordejan <jendhordejan@gmail.com>
2020-01-11 20:31:22 +0100

    added displaystats function

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -1,39 +1,4 @@
 // Write your JS here
-
-//functions: add all functions here
-//rest function : function to heal your hero
-function rest(person) {
-  console.log('you are inside rest function')
-  console.log(person.name)
-
-
-  //set character health to 10
-  if (person.health<10){
-    person.health = 10;
-      alert('Your health is full and ready to battle')
-  } else if (person.health==10){
-      alert('Your health is still full.')
-    }
-  return person;
-}
-
-
-
-//pickUpItem : function the pick-up items.
-function pickUpItem(a) {
-  /* ... */
-}
-
-//equipWeapon : function to equipt hero weapon
-function equipWeapon(a) {
-  /* ... */
-}
-
-
-
-
-
-
 //Create a hero.
 const hero = {
   name: 'Rein',
@@ -42,14 +7,95 @@ const hero = {
     type: 'Sword',
     damage: 2
   },
-  attack: 10,
+  attack: 0,
   health: 10,
   heroic: true,
   inventory: []
 }
 
-console.log(`hero health before rest: ${hero.health}`)
-//for testing
-//hero.health=8;
-rest(hero);
-console.log(hero.health)
+const innElement = document.getElementById("inn")
+const daggerElement = document.getElementById("dagger")
+
+  
+
+//functions: add all functions here
+//rest function : function to heal your hero
+function rest(person) {
+  //set character health to 10
+  if (person.health<10){
+    person.health = 10;
+    hero.health=10;
+      alert('Your health is full and ready to battle')
+  } else {
+      alert('Your health is still full')
+  }
+  return person;
+}
+
+//equipWeapon : function to equipt hero weapon
+function equipWeapon(person) {
+  /* ... */
+
+}
+
+//pickUpItem : function to pickup item.
+function pickUpItem(person,weapon){
+  console.log(`weapon in PickUpItem ${weapon}`)
+  person.inventory[person.inventory.length+1] = weapon;
+  // console.log(person.inventory[person.inventory.length-1])
+  // console.log(person.inventory[person.inventory.length-1].damage)
+  hero.inventory[hero.inventory.length] = weapon;
+  return person;
+}
+
+
+
+
+innElement.addEventListener('click', event => {
+  console.log('inn is clicked')
+  rest(hero)
+})
+
+daggerElement.addEventListener('click', event => {
+  console.log('dagger is clicked')
+  const dagger = {type: 'dagger', damage: 2}
+  pickUpItem(hero, dagger)
+})
+
+
+//function to display hero stats
+function displaystats(){
+  
+  const secElement = document.getElementById("hero stats");
+  const aNameElement = document.getElementById("heroname");
+  const aHealthElement = document.getElementById("health");
+  const aWeapElement = document.getElementById("weapon");
+  const aInventoryElement = document.getElementById("inventory");
+
+  aNameElement.innerText = `Name: ${hero.name}` 
+  aHealthElement.innerText = `Health: ${hero.health}`
+  aWeapElement.innerText = `Weapon: ${hero.weapon.type} - ${hero.weapon.damage}`
+  aInventoryElement.innerText = `Inventory: ${hero.inventory.length}`
+
+  secElement.appendChild(aNameElement);
+  secElement.appendChild(document.createElement('br'))
+  secElement.appendChild(aWeapElement);
+  secElement.appendChild(document.createElement('br'))
+  secElement.appendChild(aHealthElement);
+  secElement.appendChild(document.createElement('br'))
+  secElement.appendChild(aInventoryElement)
+
+  
+
+}
+
+displaystats()
+
+
+
+
+
+
+
+
+
